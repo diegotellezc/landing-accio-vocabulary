3ed1740543c67c6750eeda6a19b39f7ca7c531a8
Diego Tellez <diegotellezcontreras@gmail.com>
2022-11-18 16:03:00 -0500

    Updates

---
+++ main.js
@@ -0,0 +1,44 @@
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UC12Amn7rjHB4BYmA3KfULaQ&part=snippet%2Cid&order=date&maxResults=16"
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '31ac2b4044msheba08d456ba13dbp10c97ajsn2af02f3aad7f',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options)
+    const data = await response.json()
+    return data
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700" style="color:white;">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${video.snippet.title}
+                    </h3>
+                </div>
+            </div>
+        </a>
+        `).slice(0, 16).join('')}
+        `;
+        content.innerHTML = view;
+    } catch {
+     
+    }
+})();
